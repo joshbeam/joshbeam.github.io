550a65d22e46f917e87d422eaba14e01ceb8ee59
Joshua Beam <joshua.a.beam@gmail.com>
2015-05-26 19:39:54 -0500

    new post

---
+++ js/dist/github/github.js
@@ -0,0 +1,31 @@
+function populateGitHub(res) {
+  var repos = res.data,
+      popular = [];
+
+  repos.forEach(function(info) {
+    if(info.stargazers_count > 1) {
+      popular.push(info);
+    }
+  });
+
+  $('.github-title').prepend([
+    'Showing '+popular.length+' of '+repos.length+' public repos '
+  ].join(''));
+
+  popular.forEach(function(repo) {
+    $('.github').append([
+        //'<dl>',
+          '<dt>',
+            '<a href="'+repo.html_url+'">'+repo.name+'</a>',
+            ' <span>('+repo.language+', '+repo.stargazers_count+' starrers)</span>',
+          '</dt>',
+          '<dd>'+repo.description+'</dd>',
+        //'</dl>'
+      ].join(''));
+  });
+}
+
+var script = document.createElement('script');
+script.src = 'https://api.github.com/users/joshbeam/repos?callback=populateGitHub';
+
+document.getElementsByTagName('head')[0].appendChild(script);
